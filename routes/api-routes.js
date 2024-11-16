dbf78bf4735aa0904cf70cb3bb4144a1f0b785a0
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-16 18:27:41 +0700

    Update api-routes.js
    ubah by ID jadi get All

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const {getFlashcardbyID, getFlashcardbyCategory, getFlashcardbyjenisBahasa} = require ('.../controller/api/flashcardController');
-const {getQuizbyID, getQuizbyCategory, getQuizbyjenisBahasa} = require ('.../controller/api/quizController');
+const {getFlashcardAll, getFlashcardbyCategory, getFlashcardbyjenisBahasa} = require ('.../controller/api/flashcardController');
+const {getQuizAll, getQuizbyCategory, getQuizbyjenisBahasa} = require ('.../controller/api/quizController');
 const {getProfile, updateProfile} = require ('.../controller/api/profileController');
 const router = express.Router();
 
-router.get('/flashcard/:flashcardID', getFlashcardbyID);
+router.get('/flashcard', getFlashcardAll);
 router.get('/flashcard/:category', getFlashcardbyCategory);
 router.get('/flashcard/:jenisBahasa', getFlashcardbyjenisBahasa);
 
-router.get('/quiz/:quizID', getQuizbyID);
+router.get('/quiz', getQuizAll);
 router.get('/quiz/:category', getQuizbyCategory);
 router.get('/quiz/:jenisBahasa', getQuizbyjenisBahasa);
 
 router.get('/profile/:userID', getProfile);
 router.put('/profile/:userID', updateProfile);
 
-module.exports = {
-  routes: router,
-};
+module.exports = router;
