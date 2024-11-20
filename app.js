7d7c76df4d3428ba5ce4bef3ad061823469e3157
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-20 19:46:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.use(express.json());
 
 app.use("/api/FAQ", FAQRoutes);
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
