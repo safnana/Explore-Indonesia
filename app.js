9226af15e66418f0e9eb97f15b543f190e3cdf2e
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-20 19:45:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const FAQRoutes = require("./routes/FAQRoute");
+
 const app = express();
 
-
 app.use(express.json());
+
+app.use("/api/FAQ", FAQRoutes);
 
 module.exports = app;
