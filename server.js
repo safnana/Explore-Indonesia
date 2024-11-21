34af5c945cb7ef1ff397f25996797926ab340dcc
MhmmdRafli <122224190+VladR382@users.noreply.github.com>
2024-11-21 16:31:26 +0700

    Update and rename app.js to server.js
    Menambah code untuk Auth

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const admin = require("firebase-admin");
+const credentials = require("./backend-73491-firebase-adminsdk-su5he-21aa383fd3.json");
+const FAQRoutes = require("./routes/FAQRoute");
+
+const app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
+
+const db = admin.firestore();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const authRoute = require('./authRoute'); 
+app.use('/auth', authRoute);
+
+app.use(express.json());
+
+app.use("/api/FAQ", FAQRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
