1f08fbff5f1a53cefeef31ea526c5fe0fe3dcbd0
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-23 01:28:20 +0700

    Create quizRoute.js
    nambahin route quiz

---
+++ routes/quizRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { getQuizByLangType, getQuizByCategory } = require("../controllers/quizController");
+
+const router = express.Router();
+
+router.get("/:languageType", getQuizByLangType);
+router.get("/:category", getQuizByCategory);
+
+module.exports = router;
