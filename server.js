f0f45ceb5bb098d65ecd9f28a94352c21cb020ab
MhmmdRafli <122224190+VladR382@users.noreply.github.com>
2024-11-23 12:33:29 +0700

    Update server.js
    Sedikit Merapihkan Sadja

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const admin = require("firebase-admin");
-const credentials = require("./backend-73491-firebase-adminsdk-su5he-21aa383fd3.json");
 const FAQRoutes = require("./routes/FAQRoute");
 const quizRoutes = require("./routes/quizRoute");
 const flashcardsRoutes = require("./routes/flashcardsRoute");
+const authRoute = require('./routes/authRoute'); 
+const riwayatRoutes = require('./routes/riwayatRoute'); 
 
 const app = express();
-
-admin.initializeApp({
-    credential: admin.credential.cert(credentials)
-});
 
 const db = admin.firestore();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const authRoute = require('./authRoute'); 
-app.use('/auth', authRoute);
-
-app.use(express.json());
-
+app.use('/api/auth', authRoute);
+app.use('/api', riwayatRoutes);
 app.use("/api/FAQ", FAQRoutes);
 app.use("/api/quiz", quizRoutes);
 app.use("/api/flashcards", flashcardsRoutes);
