33b517f9d11bb999f7cad1e441d34a28ec007f92
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-23 23:06:34 +0700

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -7,7 +7,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 const getProfilebyID = async (req, res) => {
   const userID = req.params.userID;
   try {
-    const profile = await Profile.getuserById(userID);
+    const profile = await Profile.getProfile(userID);
     if (!profile) {
       return res.status(404).json({ message: "Profile Tidak Ditemukan" });
     }
@@ -46,7 +46,7 @@ const putProfilebyID = async (req, res) => {
       updatedAt: new Date().toISOString(),
     };
 
-    const updateProfile = await Profile.updateUserById(userID, updateData);
+    const updateProfile = await Profile.updateProfile(userID, updateData);
     res.status(200).json({ message: "Profile Berhasil Diperbarui", data: updateProfile });
   } catch (error) {
     res.status(500).json({ message: "Gagal Memperbarui Profile", error });
