be327b64049046a26f164c60fe3db70f7acb5048
MhmmdRafli <122224190+VladR382@users.noreply.github.com>
2024-11-24 16:41:28 +0700

    Create authMiddleware.js
    Menambahkan middleware di autentikasi

---
+++ Middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+const admin = require("firebase-admin");
+
+const authenticate = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(403).json({ message: "Unauthorized" });
+    }
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = decodedToken; 
+        next(); 
+    } catch (error) {
+        res.status(401).json({ message: "Token tidak valid", error: error.message });
+    }
+};
+
+module.exports = { authenticate };
