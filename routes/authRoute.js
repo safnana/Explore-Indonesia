646ae71bf77333ff4759cbac8d8d8c56c8324ca0
MhmmdRafli <122224190+VladR382@users.noreply.github.com>
2024-11-24 16:36:51 +0700

    Update authRoute.js
    menambahkan beberapa hal untuk meningkatkan keamanan dan validasi data

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
-const authController = require('./authController'); 
+const authController = require("./authController");
 
+router.post(
+    "/register",
+    [
+        body("email").isEmail().withMessage("Email tidak valid"),
+        body("password").isLength({ min: 6 }).withMessage("Password minimal 6 karakter"),
+    ],
+    authController.register
+);
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.get('/getuser', authController.getUser); 
-router.post('/logout', authController.logout);
+router.post(
+    "/login",
+    [
+        body("email").isEmail().withMessage("Email tidak valid"),
+        body("password").notEmpty().withMessage("Password wajib diisi"),
+    ],
+    authController.login
+);
+
+router.post("/logout", authController.logout);
+router.get("/getuser", authController.getUser);
 
 module.exports = router;
