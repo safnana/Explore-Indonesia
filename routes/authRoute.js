cab100408756763e25bb987e9443e880cedbe948
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-25 17:21:51 +0700

    Update authRoute.js
    update bagian path auth controller

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body } = require("express-validator");
 const router = express.Router();
-const authController = require("./authController");
+const authController = require("../controllers/authController");
 
 router.post(
     "/register",
