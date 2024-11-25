cf037dd22086cbe80ef9d49542d8ec12435f2ca6
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-25 17:21:03 +0700

    Update quizModel.js
    update model

--- models/quizModel.js
+++ models/quizModel.js
@@ -1,17 +1,21 @@
 const { db } = require("../config/db");
 
 const Quiz = {
-  getByLangType: async (languageType) => {
+  getByTypeLang: async (languageType) => {
     const snapshot = await db
       .collection("quiz")
+      .where("type", "==", "quizAkhir")
       .where("languageType", "==", languageType)
       .get();
+
     if (snapshot.empty) return [];
     return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   },
-  getByCategory: async (category) => {
+  getByTypeLangCategory: async (languageType, category) => {
     const snapshot = await db
       .collection("quiz")
+      .where("type", "==", "latihan")
+      .where("languageType", "==", languageType)
       .where("category", "==", category)
       .get();
     if (snapshot.empty) return [];
