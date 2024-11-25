81476c47e1d91054393376f7c1ce054dffa02f75
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-25 17:19:00 +0700

    Update quizController.js
    update endpoint

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,27 +1,23 @@
 const Quiz = require("../models/quizModel");
 
-const getQuizByLangType = async (req, res) => {
-  const { languageType } = req.params;
+const getQuizLatihan = async (req, res) => {
+  const { languageType, category } = req.params;
   try {
-    const quiz = await Quiz.getByLangType(languageType);
+    const quiz = await Quiz.getByTypeLangCategory(languageType, category);
     if (quiz.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "Quiz Tidak Tersedia Untuk Bahasa Ini " });
+      return res.status(404).json({ message: "Quiz Latihan Tidak Tersedia Untuk Kategori Ini " });
     }
     res.status(200).json(quiz);
   } catch (error) {
     res.status(500).json({ message: "Gagal Mengambil Data", error });
   }
 };
-const getQuizByCategory = async (req, res) => {
-  const { category } = req.params;
+const getQuizAkhir = async (req, res) => {
+  const { languageType } = req.params;
   try {
-    const quiz = await Quiz.getByCategory(category);
+    const quiz = await Quiz.getByTypeLang(languageType);
     if (quiz.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "Quiz Tidak Tersedia Untuk Category Ini " });
+      return res.status(404).json({ message: "Quiz Akhir Tidak Tersedia Untuk Bahasa Ini " });
     }
     res.status(200).json(quiz);
   } catch (error) {
@@ -29,4 +25,4 @@ const getQuizByCategory = async (req, res) => {
   }
 };
 
-module.exports = { getQuizByLangType, getQuizByCategory };
+module.exports = { getQuizLatihan, getQuizAkhir };
