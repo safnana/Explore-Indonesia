bc87a41bb557b0e0052fd6f95c482118c7addb57
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-25 17:12:47 +0700

    Update db.js
    pake serviceAccountKey

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 const admin = require("firebase-admin");
 const dotenv = require("dotenv");
+const serviceAccount = require("./serviceAdminKey.json");
 
 dotenv.config();
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
