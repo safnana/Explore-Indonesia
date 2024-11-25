713356a9fa5fa13203af39fccae061d6c9a38333
saaaa <139216836+safnana@users.noreply.github.com>
2024-11-25 17:23:51 +0700

    Update and rename server.js to app.js
    update app.js

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const admin = require("firebase-admin");
-const FAQRoutes = require("./routes/FAQRoute");
-const quizRoutes = require("./routes/quizRoute");
-const flashcardsRoutes = require("./routes/flashcardsRoute");
+const express = require("express");
+const cors = require("cors");
 const authRoute = require('./routes/authRoute'); 
-const riwayatRoutes = require('./routes/riwayatRoute'); 
+const quizRoutes = require("./routes/quizRoute");
+const FAQRoutes = require("./routes/FAQRoute");
+const flashcardsRoutes = require("./routes/flashcardsRoute");
 const profileRoutes = require("./routes/profileRoute");
+const riwayatRoutes = require('./routes/riwayatRoute'); 
+const {authenticate} = require("./middleware/authMiddleware");
 
 const app = express();
 
-const db = admin.firestore();
-
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoute);
-app.use('/api', riwayatRoutes);
-app.use("/api/FAQ", FAQRoutes);
 app.use("/api/quiz", quizRoutes);
+app.use("/api/FAQ", FAQRoutes);
 app.use("/api/flashcards", flashcardsRoutes);
 app.use("/api/profile", profileRoutes);
+app.use('/api', riwayatRoutes);
 
 app.get("/api/protected", authenticate, (req, res) => {
     res.status(200).json({ message: "Anda berhasil mengakses rute yang dilindungi", user: req.user });
