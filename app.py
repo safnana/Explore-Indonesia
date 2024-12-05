c68fb53241582869afeb75818eec01cf98706038
saaaa <139216836+safnana@users.noreply.github.com>
2024-12-05 22:16:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def translate_image():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
